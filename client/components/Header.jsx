d11d2f241295c58dbe0546ab4fe02fa08f3b02fc
GregoryProuty <g.prout009@gmail.com>
2017-02-18 15:19:34 -0800

    pulling down

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -14,23 +14,6 @@ class Header extends React.Component {
   render() {
     const { activeItem } = this.state
     return (
-        <nav className="navbar navbar-default">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <a className="navbar-brand" href="#">sMart</a>
-              <form className="navbar-form navbar-left" role="search">
-                <div className="form-group">
-                  <input type="text" className="form-control" placeholder="What are you looking for?" />
-                </div>
-                <button type="submit" className="btn btn-default">Search</button>
-              </form>              
-              <div className="navbar-right">
-              <button type="button" className="btn btn-default navbar-btn">Log in</button>
-              <a type="button" className="btn btn-default navbar-btn" href="#/PostItem">Sell Your Stuff</a>
-              </div>
-            </div>
-          </div>
-        </nav>
         <Menu>
           <Menu.Item header>SMart</Menu.Item>
           <Menu.Item name='aboutUs' active={activeItem === 'aboutUs'} onClick={this.handleItemClick} />
